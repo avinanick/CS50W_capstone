46b530402e8db4de8b16a095c2a65a9a74ecfeaa
Nick Avina <avinanick@gmail.com>
2020-10-01 16:06:57 -0700

    Changed login and logout view names so they don't clash with the django functions.

--- project_manager/urls.py
+++ project_manager/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, "index"),
-    path("login", views.login, "login"),
-    path("logout", views.logout, "logout"),
+    path("login", views.login_view, "login"),
+    path("logout", views.logout_view, "logout"),
     path("register", views.register, "register")
 ]
--- project_manager/views.py
+++ project_manager/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def index(request):
     return render(request, "project_manager/index.html")
 
-def login(request):
+def login_view(request):
     pass
 
-def logout(request):
+def logout_view(request):
     pass
 
 def register(request):
