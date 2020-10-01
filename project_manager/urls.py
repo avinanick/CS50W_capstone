82104fbf0396b73e142e082b12c56052cc5de5f9
Nick Avina <avinanick@gmail.com>
2020-09-30 22:12:56 -0700

    Created url paths for the login, logout, and register views.

--- project_manager/urls.py
+++ project_manager/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, "index"),
+    path("login", views.login, "login"),
+    path("logout", views.logout, "logout"),
+    path("register", views.register, "register")
 ]
