77369a52e1b0cbf6241213e2f8a2552e6ba83795
Nick Avina <avinanick@gmail.com>
2020-10-01 16:23:36 -0700

    Added the register user view functionality.

--- project_manager/views.py
+++ project_manager/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+
+from .models import User
 
 # Create your views here.
 
@@ -31,4 +34,27 @@ def logout_view(request):
     return redirect("login")
 
 def register(request):
-    pass
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "project_manager/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "project_manager/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return redirect("index")
+    else:
+        return render(request, "project_manager/register.html")
