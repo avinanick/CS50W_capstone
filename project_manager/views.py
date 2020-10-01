00e9de94f2930e6613ec3a4abf48b992ba940ce6
Nick Avina <avinanick@gmail.com>
2020-09-30 22:11:30 -0700

    Index view now renders the empty index page. Login, logout, and register view stubs created.

--- project_manager/views.py
+++ project_manager/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    return render(request, "project_manager/index.html")
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
+def register(request):
     pass
