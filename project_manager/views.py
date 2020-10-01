ba714d2cd78515c512c2df3336beccff05ee4bb0
Nick Avina <avinanick@gmail.com>
2020-10-01 16:20:10 -0700

    Added basic functionality to login and logout views.

--- project_manager/views.py
+++ project_manager/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
@@ -8,10 +8,27 @@ def index(request):
     return render(request, "project_manager/index.html")
 
 def login_view(request):
-    pass
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            return render(request, "project_manager/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "project_manager/login.html")
 
 def logout_view(request):
-    pass
+    logout(request)
+    return redirect("login")
 
 def register(request):
     pass
