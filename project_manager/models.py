7eef77c32e9f05a5b1ef740e3ef4b119a0a4a3ab
Nick Avina <avinanick@gmail.com>
2020-10-01 21:51:02 -0700

    Added all the fields I can currently think of for the needed models.

--- project_manager/models.py
+++ project_manager/models.py
@@ -4,3 +4,29 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Project(models.Model):
+    creator = models.ForeignKey("User", on_delete=models.CASCADE, related_name="projects")
+    name = models.CharField()
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+class Deadline(models.Model):
+    project = models.ForeignKey("Project", on_delete=models.CASCADE, related_name="deadlines")
+    due_date = models.DateTimeField()
+
+class Task(models.Model):
+    creator = models.ForeignKey("User", on_delete=models.CASCADE, related_name="tasks")
+    project = models.ForeignKey("Project", on_delete=models.CASCADE, related_name="tasks")
+    deadline = models.ForeignKey("Deadline", null=True, on_delete=models.SET_NULL, related_name="tasks")
+    title = models.CharField()
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+class Authority(models.Model):
+    level = models.CharField()
+
+class Membership(models.Model):
+    member = models.ForeignKey("User", on_delete=models.CASCADE, related_name="memberships")
+    project = models.ForeignKey("Project", on_delete=models.CASCADE, related_name="members")
+    auth_level = models.ForeignKey("Authority", on_delete=models.PROTECT, related_name="level_members")
