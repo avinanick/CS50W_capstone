e101be3e6261693296a2e28bb87d89ac681376a4
Nick <avinanick@gmail.com>
2020-10-14 17:04:50 -0700

    Registered all current models to the admin site.

--- project_manager/admin.py
+++ project_manager/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Project, Membership, Deadline, Task, Authority
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Project)
+admin.site.register(Deadline)
+admin.site.register(Task)
+admin.site.register(Authority)
+admin.site.register(Membership)
