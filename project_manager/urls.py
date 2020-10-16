f7bc70987dd0b5391cbd4b8f534581dadcaaa808
Nick <avinanick@gmail.com>
2020-10-16 16:09:45 -0700

    Added the api to get the list of projects to the urls.

--- project_manager/urls.py
+++ project_manager/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("create_project", views.create_project, name="create_project"),
+    path("get_projects", views.projects, name="get_projects"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
