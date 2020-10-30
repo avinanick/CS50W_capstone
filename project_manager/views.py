70802771b277c9ddda9bf8e8eac5a52fd53fdb03
Nick <avinanick@gmail.com>
2020-10-30 16:16:00 -0700

    Added a member api response to trying to remove a member from a project.

--- project_manager/views.py
+++ project_manager/views.py
@@ -141,7 +141,14 @@ def members(request, project_id):
                     auth_level=Authority.objects.get(level="Member"))
                 new_membership.save()
                 return JsonResponse({"message": "Member added to project"}, status=201)
-            
+        
+        if data["member_edit"] == "remove":
+            target_membership = Membership.objects.get(project=Project.objects.get(id=project_id), member=User.objects.get(username=data["username"]))
+            if target_membership.auth_level.level == "Manager" and member_check.auth_level.level != "Owner":
+                return JsonResponse({"error": "No authority for action"}, status=400)
+            target_membership.delete()
+            return JsonResponse({"message": "Member removed from project"}, status=201)
+
     all_members = Membership.objects.filter(project=requested_project)
     return JsonResponse({"members": [{
         "name": member.member.username, 
